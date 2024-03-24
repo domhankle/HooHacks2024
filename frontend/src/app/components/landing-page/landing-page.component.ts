f9ad7570ae641941ff55606055af78c6dc4bd3ce
Dom Hankle <domhankle03@gmail.com>
2024-03-24 10:41:27 -0400

    Animations!

--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -11,11 +11,24 @@ import { Router } from '@angular/router';
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
-  
-  constructor(@Inject(Router) private _router: Router)
-  {}
+  public animateOwl: boolean = false;
+  public animateHeader: boolean = false;
+  constructor(@Inject(Router) private _router: Router) {
+  }
 
   public goToSignIn(): void {
-    this._router.navigate(['sign-in']);
+    this.animateOwl = true;
+
+    setTimeout(() => {
+      this.animateOwl = false;
+      this.animateHeader = true;
+      setTimeout(() => {
+        this.animateHeader = false;
+        this._router.navigate(['sign-in']);
+      }, 500)
+
+
+    }, 500)
+
   }
 }
--- frontend/src/app/components/sign-in-page/sign-in-page.component.ts
+++ frontend/src/app/components/sign-in-page/sign-in-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { Material } from '../../utility/modules';
 import { LoginService } from '../../services/login.service';
 import { SingletonService } from '../../services/singleton.service';
@@ -11,9 +11,10 @@ import { Router } from '@angular/router';
   templateUrl: './sign-in-page.component.html',
   styleUrl: './sign-in-page.component.scss'
 })
-export class SignInPageComponent {
+export class SignInPageComponent implements OnInit {
   public username = '';
   public password = '';
+  public loaded: boolean = false;
 
   constructor(@Inject(LoginService) private _loginService: LoginService,
     @Inject(SingletonService) private _singletonService: SingletonService,
@@ -21,14 +22,23 @@ export class SignInPageComponent {
 
   }
 
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.loaded = true;
+    }, 100)
 
+  }
   public signIn(): void {
     this._loginService.getDoctorUser(this.username, this.password).subscribe(
       response => {
         const doctor = { id: response.doctor.id, username: response.doctor.username, password: response.doctor.password, patients: response.doctor.patients };
         this._singletonService.currentDoctor.next(doctor);
         console.log("Currently Signed in User: ", this._singletonService.currentDoctor.value);
-        this._router.navigate(['dashboard']);
+        this.loaded = false;
+        setTimeout(() => {
+          this._router.navigate(['dashboard']);
+        }, 600);
+
       },
       error => {
         console.log(error);
