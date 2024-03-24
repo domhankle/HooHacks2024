d50cdc0b87b3c30a8ca2d3ac589722a6206f8498
Victor Londono <vlondon2@gmu.edu>
2024-03-24 03:07:31 -0400

    adding placeholder patients

--- backend/healthapp/service/users/endpoints.py
+++ backend/healthapp/service/users/endpoints.py
@@ -10,6 +10,210 @@ def is_patient(user):
 def is_doctor(user):
     return hasattr(user, 'doctor')
 
+def fake_patients():
+    return [{
+        'name': 'Domenic Hankle',
+        'email': 'dhank003@gmail.com',
+        'phone_number': '757-394-6969',
+        'appointments':
+        [{
+            'date': 3020302,
+            'doctor': 'Dr. T-Pain',
+            'complete': True,
+            'vitals':
+            {
+                'weight': 8000,
+                'height': 4
+            },
+            'notes': 'Bro took a shit on the hospital bed.',
+            'reason_for_visit': 'Excessive peeing'
+        },
+        {
+            'date': 289342,
+            'doctor': 'Dr. Penis',
+            'complete': False,
+            'vitals':{},
+            'notes': 'wtfff.',
+            'reason_for_visit': 'Drank Norfolk water.'
+        }],
+        'prescriptions': 
+        [{
+            'name': 'Comethazine',
+            'start': 0,
+            'end': 1000000,
+            'refill': True
+        },
+        {
+            'name': 'Soggy Crispers',
+            'start': 33,
+            'end': 489489,
+            'refill': False
+        }],
+        'immunizations': 
+        [{
+            "name": "Rabies",
+            "up_to_date": False,
+            "expires": 567
+        },
+        {
+            "name": "running out of ideas",
+            "up_to_date": True,
+            "expires": 78483789743
+        }]
+
+    },
+    {
+        'name': 'Anthony Vecchio',
+        'email': 'dapie125@gmail.com',
+        'phone_number': '757-339-7969',
+        'appointments':
+        [{
+            'date': 3020302,
+            'doctor': 'Dr. T-Pain',
+            'complete': True,
+            'vitals':
+            {
+                'weight': 0,
+                'height': 59
+            },
+            'notes': 'fiended over black women.',
+            'reason_for_visit': 'homosexual tendencies'
+        },
+        {
+            'date': 289342,
+            'doctor': 'Dr. Penis',
+            'complete': False,
+            'vitals':{},
+            'notes': 'wtfff.',
+            'reason_for_visit': 'eat without youtube.'
+        }],
+        'prescriptions': 
+        [{
+            'name': 'PeanutButterPretzels',
+            'start': 0,
+            'end': 1000000,
+            'refill': True
+        },
+        {
+            'name': 'Black Pussy',
+            'start': 33,
+            'end': 489489,
+            'refill': False
+        }],
+        'immunizations': 
+        [{
+            "name": "idk",
+            "up_to_date": False,
+            "expires": 567
+        },
+        {
+            "name": "bruhhhhh",
+            "up_to_date": True,
+            "expires": 78483789743
+        }]
+    },
+    {
+        'name': 'Grady Insley',
+        'email': 'dagradster@gmail.com',
+        'phone_number': '757-696-9988',
+        'appointments':
+        [{
+            'date': 3020302,
+            'doctor': 'Dr. Penja Mon',
+            'complete': True,
+            'vitals':
+            {
+                'weight': 200000,
+                'height': 200
+            },
+            'notes': 'Worked on dnd campaign all appt.',
+            'reason_for_visit': 'homosexual tendencies'
+        },
+        {
+            'date': 289342,
+            'doctor': 'Dr. Penis',
+            'complete': False,
+            'vitals':{},
+            'notes': 'wtfff.',
+            'reason_for_visit': 'eat without youtube.'
+        }],
+        'prescriptions': 
+        [{
+            'name': 'Lean',
+            'start': 0,
+            'end': 1000000,
+            'refill': True
+        },
+        {
+            'name': 'Toke',
+            'start': 33,
+            'end': 489489,
+            'refill': False
+        }],
+        'immunizations': 
+        [{
+            "name": "HIV/AIDS",
+            "up_to_date": False,
+            "expires": 567
+        },
+        {
+            "name": "Flu",
+            "up_to_date": True,
+            "expires": 78483789743
+        }]
+    },
+    {
+        'name': 'Ross Insley',
+        'email': 'ilovedestiny@gmail.com',
+        'phone_number': '757-idkmf',
+        'appointments':
+        [{
+            'date': 3020302,
+            'doctor': 'Dr. Sukuna',
+            'complete': True,
+            'vitals':
+            {
+                'weight': 200000,
+                'height': 200
+            },
+            'notes': 'what do i even write here.',
+            'reason_for_visit': 'also homosexual tendencies'
+        },
+        {
+            'date': 289342,
+            'doctor': 'Dr. Penis',
+            'complete': False,
+            'vitals':{},
+            'notes': 'kms.',
+            'reason_for_visit': 'valorant player.'
+        }],
+        'prescriptions': 
+        [{
+            'name': 'vitaminwater',
+            'start': 0,
+            'end': 1000000,
+            'refill': True
+        },
+        {
+            'name': 'everclear',
+            'start': 33,
+            'end': 489489,
+            'refill': False
+        }],
+        'immunizations': 
+        [{
+            "name": "random shit",
+            "up_to_date": False,
+            "expires": 567
+        },
+        {
+            "name": "Flu",
+            "up_to_date": True,
+            "expires": 78483789743
+        }]
+    }]
+
+
 @csrf_exempt
 def get_doctor(request):
     username = request.GET.get('username')
@@ -34,7 +238,7 @@ def get_doctor(request):
             'id': user.id,
             'username': user.username,
             'password': user.password,
-            'patients': []
+            'patients': fake_patients()
         }
 
 
